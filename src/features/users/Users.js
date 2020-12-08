4ad349c8cbc562cdb7447311e52655247c1cc638
ihollander <ianholla@gmail.com>
2020-12-08 10:00:19 -0500

    Added solution

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,14 +1,18 @@
 import React from "react";
-// add any needed imports here
+import { useSelector } from "react-redux";
 
 function Users() {
+  const users = useSelector((state) => state.users);
+  const userCount = useSelector((state) => state.users.length);
+
   return (
     <div>
       <ul>
-        Users!
-        {/* Write code here that displays the usernames of all users in the Redux store */}
-        {/* In addition, display the total number of users curently in the store */}
+        {users.map((user) => (
+          <li key={user.username}>{user.username}</li>
+        ))}
       </ul>
+      Total Users: {userCount}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./App";
 
 import usersReducer from "./features/users/usersSlice";
 
-// add imports and code
+const store = createStore(usersReducer);
 
 ReactDOM.render(
-  // add imports and code
-  <App />,
-  // add imports and code
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
