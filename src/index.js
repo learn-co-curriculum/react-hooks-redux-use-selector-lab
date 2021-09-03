af58628109c183436685ab620c1029577006142c
noora <noorayaqub44@gmail.com>
2021-09-03 17:48:44 +0300

    commit tree 39fb45a2c3bd825eac80535fb9363c5e7a26c9a8

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import usersReducer from "./features/users/usersSlice";
 
 // add imports and code
+const store = createStore(
+  usersReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
-  // add imports and code
-  <App />,
+  <Provider store={store}>
+  <App />
+  </Provider >,
   // add imports and code
   document.getElementById("root")
 );
